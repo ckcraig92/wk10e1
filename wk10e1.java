554a24104b3e5c7829818a2248e4716cfc886d0c
ckcraig@amarillocollege.com <ckcraig@amarillocollege.com>
2025-03-31 21:06:06 -0500

    update

---
+++ wk10e1.java
@@ -0,0 +1,64 @@
+import java.utl.Scanner;
+
+class Main
+
+{
+    public static void(String[] args)
+}
+
+
+{
+    //menu
+    System.out.println("Voting Menu");
+    System.out.println("1. Cast Vote");
+    Systen.out.println("2. Display Results");
+    System.out.println("3. Reset Votes");
+    System.out.println("4. Exit");
+    System.out.println("Choose a menu option: ");
+
+}
+
+
+static void CastVote = (string candidateName);
+static void DisplayResults()
+static void ResetVotes()
+switch (userInput)
+{
+    case 1:
+    CastVote(candidateName);
+    break;
+    case 2:
+    DisplayResults();
+    break;
+    case 3:
+    ResetVotes();
+    break;
+    case 4:
+    Exit;
+    break;
+}
+
+
+
+    //method to cast votes
+    public static void CastVote(string candidateName)
+    {
+        //process
+        System.out.println("Vote casted for:" + candidateName ); //screen for user to view vote
+    }
+
+    //method to display votes
+    public static void DisplayResults()
+    {
+        //process
+        System.out.println("Results:" + DisplayResults() ); //screen for user to view results
+    }
+
+    //method to reset
+    public static void ResetVotes()
+    {
+        //process
+        System.out.print( "Reset occurred" ) //screen for user to view reset
+    }
+}
+
